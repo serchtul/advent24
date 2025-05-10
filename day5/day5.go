416cce6765095caf4a01969bbe0a512dd2e5757a
Sergio García <3311638+serchtul@users.noreply.github.com>
2025-05-09 21:39:00 -0600

    complete day 5 solution

--- day5/day5.go
+++ day5/day5.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -13,7 +14,6 @@ var existsStruct struct{}
 
 func main() {
 	file, err := os.Open("input.txt")
-
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,10 +24,9 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		line := scanner.Text()
-		pages := strings.Split(line, "|")
+		pages := strings.Split(scanner.Text(), "|")
 
-		// We stop whenever we encounter an empty line
+		// The dependency list ends whenever we encounter an empty line
 		if len(pages) == 1 {
 			break
 		}
@@ -42,41 +41,61 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Sequencing requirements", sequencing)
 
-	fmt.Println("Requirements", sequencing)
-
-	result := 0
+	firstTryResults := 0
+	reorderedResults := 0
 	for scanner.Scan() {
-		line := scanner.Text()
-		updates := strings.Split(line, ",")
-		seenPages := make(map[string]struct{})
+		pageUpdates := strings.Split(scanner.Text(), ",")
 
-		validOrder := true
-		for _, pageNo := range updates {
-			fmt.Println("seen", seenPages, "current page", pageNo)
+		firstTry := true // Whether the sequence was valid on the first check
+		var validOrder bool
+		for {
+			var idx int
+			var page string
+			var conflictingIdx int
 
-			for seen := range seenPages {
-				// The sequence is not in the correct order, because we saw a page we should have seen *after* this one
-				if _, exists := sequencing[pageNo][seen]; exists {
-					fmt.Println("Invalid sequence order", line)
+			validOrder = true
+			for idx, page = range pageUpdates {
+				// Find whether there's a conflicting page (one that should be after the current one, instead of before)
+				conflictingIdx = slices.IndexFunc(pageUpdates[:idx], func(p string) bool {
+					_, conflictExists := sequencing[page][p]
+					return conflictExists
+				})
+
+				if conflictingIdx != -1 {
+					fmt.Println("failed at page", page, "| seen pages", pageUpdates[:idx])
+
 					validOrder = false
+					firstTry = false
 					break
 				}
 			}
-			seenPages[pageNo] = existsStruct
-			if !validOrder {
+
+			if validOrder {
+				fmt.Println("Valid sequence order", pageUpdates)
 				break
 			}
+
+			fmt.Println("Invalid sequence order", pageUpdates)
+			fmt.Println(page, "must be before", pageUpdates[conflictingIdx], "on index", conflictingIdx)
+
+			// Re-arrange current item to be before its first dependency
+			pageUpdates = slices.Delete(pageUpdates, idx, idx+1)
+			pageUpdates = slices.Insert(pageUpdates, conflictingIdx, page)
+			fmt.Println("rearranged conflicting update", pageUpdates)
 		}
 
-		if validOrder {
-			fmt.Println("Valid sequence order", line)
-			// Assumes the number will correctly be parsed.
-			// This also assumes the number of updates is always odd
-			middle, _ := strconv.Atoi(updates[(len(updates)-1)/2])
-			result += middle
+		// Assumes the number in the file will correctly be parsed.
+		// This also assumes the number of updates is always odd
+		middle, _ := strconv.Atoi(pageUpdates[(len(pageUpdates)-1)/2])
+		if firstTry {
+			firstTryResults += middle
+		} else {
+			reorderedResults += middle
 		}
 	}
 
-	fmt.Println("Result is", result)
+	fmt.Println("First-try sequences sum", firstTryResults)
+	fmt.Println("Reordered sequences sum", reorderedResults)
 }
